1513d48f7e6f819afcc35928dfe385349923a295
LucasCto <134112800+LucasCto@users.noreply.github.com>
2023-05-29 20:46:36 -0300

    Delete Archivos JSON directory

--- Archivos JSON/main.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("data.json") as f:
-    data = json.load(f)
-
-    direccion = data["direccion"]
-    calle = direccion["calle"]
-    print("Direccion: ", direccion)
-    print("Calle: ", calle)
