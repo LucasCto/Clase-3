c0d4ddf76f83885e9f53427e76dcb4d87faf8b07
LucasCto <1lucascastillox123@gmail.com>
2023-05-29 20:41:44 -0300

    Added 2 modulos nuevos

---
+++ main.py
@@ -0,0 +1,42 @@
+#Contexto para usar el archivo csv: para poder encontrar si fuera un policia y quiero hacer una multa
+#donde esta radicado el vehiculo y tambien quien es el dueño
+#tambien sirve para buscar por patentes en caso de solo tener la patente del vehiculo
+#y otra utilidad es la de añadir nuevas patentes en caso de que esta no hayan sido añadidas al sistema
+#tambien si el dueño no es el correcto poder modificar dicho dato
+#que me haga un archivo nuevo con todas la informacion, ingresando una provinicia
+import numpy as np
+import pandas as pd
+import csv
+
+def buscador_por_patente(data):
+    patente = input("Ingrese la patente: ")
+    vector = np.asarray(data[:,1])
+    for datos in vector:
+        if datos == patente:
+            print("Patente encontrada")
+            print(datos)
+        else:
+            print("Disculpe, no se encontró la patente")
+            
+def nuevo_ingreso():
+    nombre = input("Ingres nombre y apellido (ej, Maria Paz): \n")
+    patente = input("Ingrese patente: ")
+    provincia = input("Ingrese la ubicación de donde esta radicado el automotor: \n")
+    id = len(data)
+    with open('patentes.csv') as nuevo:
+        nuevo.write(f'0{id + 1},{patente},{nombre},{provincia}')
+
+def eliminar_patente():
+    patentes = np.asarray(data)
+    buscador = input("Ingrese la patente a eliminar: ")
+    resultado = np.where(patentes == buscador)
+    if len(resultado[0]) > 0:
+        indice_encontrado = resultado[0][0]
+        print(f"El valor {buscador} fue encontrado en el índice {indice_encontrado}.")
+        matriz_nueva = np.delete(patentes, indice_encontrado, axis=0)
+        with open('patentes.csv', 'w', newline='') as archivo:
+            escritor_csv = csv.writer(archivo)
+            for fila in matriz_nueva:
+                escritor_csv.writerow(fila)
+    else:
+        print(f"El valor {buscador} no fue encontrado en el arreglo.")
