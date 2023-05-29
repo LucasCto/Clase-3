5336b038cf5af9d718cfcc18a99e96a00692515e
LucasCto <134112800+LucasCto@users.noreply.github.com>
2023-05-29 20:45:49 -0300

    Delete Archivos CSV directory

--- Archivos CSV/main-reader.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open("dummy-data.csv", "r") as f:
-    reader = csv.reader(f)
-    suma = 0
-    for row in reader:
-        suma += int(row[1])
-
-    print("promedio de edades:", suma / 4)
